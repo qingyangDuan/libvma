5dd17757024d44736f1512754f6b3000772c8c9e
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2021-05-24 00:37:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,18 @@ sudo /etc/init.d/openibd restart
 # 此时 libvma.so* 已被安装在 /usr/lib 中， 但这不是我们要用的
 
 
+Prerequisites:
+sudo apt install -y libnl-3-dev
+sudo apt install -y libnl-route-3-dev
+sudo vim /etc/security/limits.conf
+#add line :   duanqingyang hard memlock unlimited
+relink ths ssh shell
+#then use " ulimit -a " to check if it works
+
 
 2)build libvma
 git clone https://github.com/Mellanox/libvma
 cd libvma
-sudo apt install -y libnl-3-dev
-sudo apt install -y libnl-route-3-dev
 ./autogen.sh
 ./configure --with-ofed=/usr --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include --docdir=/usr/share/doc/libvma --sysconfdir=/etc
 make
